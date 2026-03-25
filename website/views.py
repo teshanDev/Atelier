feb9ad3fcf9199cf3cfd3e33363ebb95393b5fef
IT24102362 <mathinthateshan@gmail.com>
2026-03-26 02:19:43 +0530

    Added new Feature: Upload images

--- website/views.py
+++ website/views.py
@@ -1,36 +1,40 @@
-from flask import Blueprint, render_template, request, flash, json, jsonify
-import json
-from flask_login import login_required,current_user
-
-from website import db
-from website.models import Sketch
+import os
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from werkzeug.utils import secure_filename
+from . import db
+from .models import Sketch
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET','POST'])
+
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
-        note = request.form.get('note')
+        file = request.files.get('sketch_image')
 
-        if len(note) < 1 :
-            flash('Note is too short.', category='error')
+        if file and file.filename != '':
+            filename = secure_filename(file.filename)
+
+            # Use this standard way to find your folders
+            # This looks for the 'static/uploads' folder relative to this file
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            upload_folder = os.path.join(base_dir, 'static', 'uploads')
+
+            if not os.path.exists(upload_folder):
+                os.makedirs(upload_folder)
+
+            file_path = os.path.join(upload_folder, filename)
+            file.save(file_path)
+
+            new_sketch = Sketch(image_path=filename, user_id=current_user.id)
+            db.session.add(new_sketch)
+            db.session.commit()
+            flash('Image Uploaded!', category='success')
         else:
-            new_note = Sketch(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added.', category='success')
+            flash('No image selected!', category='error')
 
-    return render_template("home.html", user=current_user, all_notes=Sketch.query.all())
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Sketch.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+    # Get all sketches to display on the home page
+    all_sketches = Sketch.query.all()
+    return render_template("home.html", user=current_user, all_notes=all_sketches)
