63317d767896208e7749d04d281d6f4a7af43847
IT24102362 <mathinthateshan@gmail.com>
2026-04-04 12:33:53 +0530

    updated some UI features

--- website/static/index.js
+++ website/static/index.js
@@ -8,12 +8,40 @@ function deleteNote(noteId) {
 }
 
 function like(sketchId) {
+    const likeIcon = document.getElementById(`like-icon-${sketchId}`);
     const likeCount = document.getElementById(`likes-count-${sketchId}`);
 
     fetch(`/like-sketch/${sketchId}`, { method: "POST" })
         .then((res) => res.json())
         .then((data) => {
             likeCount.innerHTML = data["likes"];
+
+            if (data["liked"] === true) {
+                likeIcon.classList.remove("far");
+                likeIcon.classList.add("fas");
+            } else {
+                likeIcon.classList.remove("fas");
+                likeIcon.classList.add("far");
+            }
         })
-        .catch((e) => alert("Could not like sketch."));
+        .catch((e) => console.error("Error liking sketch:", e));
 }
+
+function previewImage(event) {
+    const reader = new FileReader();
+    reader.onload = function() {
+        const output = document.getElementById('output-preview');
+        output.src = reader.result;
+        output.style.display = 'block';
+    };
+    reader.readAsDataURL(event.target.files[0]);
+}
+
+setTimeout(function() {
+    let alerts = document.querySelectorAll('.alert');
+    alerts.forEach(function(alert) {
+        alert.style.transition = "opacity 0.5s ease";
+        alert.style.opacity = "0";
+        setTimeout(()=> alert.remove(), 500);
+    });
+}, 3000);
